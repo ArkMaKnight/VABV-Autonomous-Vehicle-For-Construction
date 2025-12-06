fb6f1efa2eed904c5de0c08de4329a1b066e18fa
Arturo Marín <contact.artmarin401@gmail.com>
2025-12-06 13:10:40 -0500

    (refactor): Limpiar legibilidad de código de entrenamiento.

--- AI_BRAIN_Laptop/train_once.py
+++ AI_BRAIN_Laptop/train_once.py
@@ -5,6 +5,7 @@ import os
 import torch
 
 # Configuraciones 
+iteraciones = 0
 workspace = "tesi-intelligent-autonomous-smart-vehicle-for-construction"
 project = "epp-autonomous-vehicle-construction"
 tarjeta_grafica = 0
@@ -14,25 +15,27 @@ tarjeta_grafica = 0
 load_dotenv()
 ROBOFLOW_KEY = os.getenv("ROBOFLOW_KEY")
 
-# Verificamos que use RTX4060
-print("¿CUDA disponible?:", torch.cuda.is_available())
+if iteraciones == 0: 
+# Verificamos que use la tarjeta gráfica asignada RTX4060
+    print("¿CUDA disponible?:", torch.cuda.is_available())
+    if torch.cuda.is_available():
+        print("¡ÉXITO! Ahora usando:", torch.cuda.get_device_name(tarjeta_grafica))
+        print("Versión de CUDA:", torch.version.cuda)
+    else:
+        print("Sigue saliendo CPU... avísame si pasa esto.")
+    if not ROBOFLOW_KEY: 
+        print("API no encontrada. Descartando cambios...")
 
-if not ROBOFLOW_KEY: 
-    print("API no encontrada. Descartando cambios...")
-
+print("Número de iteraciones: ", iteraciones)
 #2: Descargar el Dataset para Carrito :) 
-
-if torch.cuda.is_available():
-    print("¡ÉXITO! Ahora usando:", torch.cuda.get_device_name(tarjeta_grafica))
-    print("Versión de CUDA:", torch.version.cuda)
-else:
-    print("Sigue saliendo CPU... avísame si pasa esto.")
 
 rf = Roboflow(api_key=ROBOFLOW_KEY)
 project = rf.workspace(workspace).project(project)
 dataset = project.version(1).download("yolov8")
 
 # 3. Entrenar el Modelo
+    #batch=16 -> En caso, se tenga más memoria RAM.
+
 if __name__ == '__main__':
     model = YOLO("yolov8n.pt")
     print("Comienza el Entrenamiento 🚗...")
@@ -42,7 +45,9 @@ if __name__ == '__main__':
         epochs=50,
         imgsz=640,
         device = tarjeta_grafica,
-        batch=16,
+        batch=8,
+        workers=2,
+        cache=False,
         name='modelo_tesiproject_v1'
     )
 
--- test.py
+++
@@ -1,1 +0,0 @@
-print("Características de Torch")
