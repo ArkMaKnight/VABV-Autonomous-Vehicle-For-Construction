e6b8e0661f6d87db1d03a4470aac86c44087ecfe
Arturo Marín <contact.artmarin401@gmail.com>
2026-02-11 23:56:10 -0500

    refactor: dependeces changed

---
+++ AI_BRAIN_Laptop/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    root: '/src',
+    build: {
+        outDir: '../static/dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: './main.js',
+            output: {
+                entryFileNames: 'bundle.js'
+            }
+        },
+    },
+    server: {
+        port: 5173,
+        cors: true
+    }
+})
